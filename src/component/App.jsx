29ceda9a41630df2bcf8b29ed1c9e26cb8dad4b7
Prakhar <prakharswarnkar29@gmail.com>
2023-06-23 22:57:31 +0530

    Initial commit

---
+++ src/component/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function App() {
+
+    function handleClick {
+
+    }
+    return {
+        <div className="container">
+          <h1>Hello $PlayerName$</h1>
+          <input type="text" placeholder="Enter your password" />
+          <button onClick={handleClick}>Submit</button>
+        </div>
+    };
+}
+
+export default App;
